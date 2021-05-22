71c4f93ab3dfb94721d62affe53e0707ea9d0d81
Selozhd <zahidbalcioglu@gmail.com>
2021-05-22 06:55:28 -0400

    masking added

--- fnet.py
+++ fnet.py
@@ -13,6 +13,7 @@ class Forward(tf.keras.layers.Layer):
         super(Forward, self).__init__(**kwargs)
         self.units = units
         self.rate = dropout_rate
+        self.supports_masking = True
         self.dense1 = tf.keras.layers.Dense(units, activation=tf.nn.gelu)
         self.dense2 = tf.keras.layers.Dense(units)
         self.dropout = tf.keras.layers.Dropout(self.rate)
@@ -35,6 +36,7 @@ class FNetBlock(tf.keras.layers.Layer):
     def __init__(self, hidden_dim):
         super(FNetBlock, self).__init__()
         self.hidden_dim = hidden_dim
+        self.supports_masking = True
         self.norm_fourier = tf.keras.layers.BatchNormalization()
         self.norm_ffn = tf.keras.layers.BatchNormalization()
         self.ffn = Forward(self.hidden_dim, 0.1)
@@ -48,6 +50,6 @@ class FNetBlock(tf.keras.layers.Layer):
         X_complex = tf.cast(inputs, tf.complex64)
         X_fft = tf.math.real(tf.signal.fft2d(X_complex))
         X_norm1 = self.norm_fourier(X_fft + inputs, training=training)
-        X_dense = self.ffn(X_norm1)
+        X_dense = self.ffn(X_norm1, training=training)
         X_norm2 = self.norm_ffn(X_dense + X_norm1, training=training)
         return X_norm2
